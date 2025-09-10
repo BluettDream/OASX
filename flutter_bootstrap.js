15837d2b775b8b417bae9e62b07d2151103d57ce
BluettDream <bluetian1st@gmail.com>
2025-09-10 04:58:22 +0000

    Deploying to page from @ BluettDream/OASX@d01c49385c980a3520382ce2e85bc6295e8d4db7 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2497084353"
+    serviceWorkerVersion: "4234490309"
   }
 });
